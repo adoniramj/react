1e38392dcbe42ea60f7b93d97a671396d69424a7
adoniramj <adoniramjvargas@gmail.com>
2019-12-10 14:59:25 -0500

    Modify App.js - Create an input and button to insert a new item to the todo list. - Create a function that checks that the new item does not exist in the list. If it does do not update the list.

--- project_1/src/App.js
+++ project_1/src/App.js
@@ -7,15 +7,45 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userName: "Adam"
+      userName: "Adam",
+      todoItems: [
+        {action: 'Buy flowers', done: false},
+        {action: 'Get shoes', done: false},
+        {action: 'Collect tickets', done: true},
+        {action: 'Call mom', done:false}
+      ],
+      newItemText: ''
     }
   }
+
+  updateNewTextValue = (event) => {
+    this.setState({newItemText: event.target.value})
+  }
+
+  createNewTodo = () => {
+    if (!this.state.todoItems.find(item => item.action === this.state.newItemText)) {
+      this.setState({
+        todoItems: [...this.state.todoItems, {action: this.state.newItemText, done: false}],
+        newItemText: ''
+      })
+    }
+  }
+
   render() {
     return (
       <div>
         <h4 className="bg-primary text-white text-center p-2">
-          To do List
+          {this.state.userName}'s To do List
         </h4>
+        <div className='container-fluid'>
+          <div className='my-1'>
+            <input className='form-control'
+                   value={ this.state.newItemText }
+                   onChange={ this.updateNewTextValue } />
+            <button className='btn btn-primary mt-1'
+                    onClick={ this.createNewTodo}>Add</button>
+          </div>
+        </div>
       </div>    
     );
   }
